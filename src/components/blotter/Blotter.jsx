9fdc83e1d0400460d4cabc9a9c3c71e6044fab30
vladimir <vladimir@notebook>
2017-07-07 14:22:46 +0400

    test 3

--- src/components/blotter/Blotter.jsx
+++ src/components/blotter/Blotter.jsx
@@ -4,20 +4,42 @@ const ApiSvc = require('../../../assets/js/services/tradingServer/apiSvc.js');
 
 const Blotter = () => {
     
+    function showMessage(isLogged){
+        if (isLogged){
+            alert("you are logged");
+        }else{
+            alert("you aren't logged");
+        }
+    }
+    
     function testClick (){
-            ApiSvc.getAccountPositions(this.state.account).then( res => {
-                positionList = res.body;
-                console.log('position list');
-                console.log(positionList);
-            });            
+        var isLogged = ApiSvc.isAlreadyLogged();
+        showMessage(isLogged);
+//            ApiSvc.getAccountPositions(3).then( res => {
+//                positionList = res.body;
+//                console.log('position list');
+//                console.log(positionList);
+//            });            
     };    
+    function login(){
+        if (ApiSvc.isAlreadyLogged()) {
+            console.log("already logged");
+            showMessage(true);
+            return;
+        }
+            
+        var succ = ApiSvc.login('test', 'test');
+        showMessage(succ); 
+    };
+    
     
   return (
     <div className="panel panel-default">
       <div className="panel-heading">BLOTTER</div>
       <div className="panel-body">
         Panel content
-        <button className="btn" onClick={testClick}>test get data</button>
+        <button className="btn" onClick={testClick}>test log state</button>
+        <button className="btn" onClick={login}>do log in as test</button>         
       </div>
     </div>
   );
